bdd6267b84155eed38f0d23b327ad3c161adc8ff
Uma Sudheer Satyawada <sudheer1293@gmail.com>
2020-11-11 19:38:49 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AddUpdateMember from './components/AddUpdateMember';
+// import UserRegistration from './components/UserRegistration';
+// import UserDataTable from './components/UserDataTable';
+import UserCreation from './components/UserCreation';
+// import Login from './components/Login';
+import MemberDataTable from './components/MemberDataTable';
+import LoginCreation from './components/LoginCreation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+         
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={LoginCreation} />
+            <Route exact path="/login" component={LoginCreation} />
+            <Route exact path="/home" component={MemberDataTable} />
+            <Route exact path="/register-user" component={UserCreation} />
+            <Route exact path="/add-member" component={AddUpdateMember} />
+            <Route exact path="/edit-member/:id" component={AddUpdateMember} />
+          </Switch>
+        </BrowserRouter>
+    );
+  }
 }
 
 export default App;
